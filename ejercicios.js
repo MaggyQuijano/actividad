f0d846792a1cd1cea14c6d7bd29cc78413d6cd1e
MaggyQuijano <magali.quijano.pliego@gmail.com>
2024-01-11 13:04:37 -0600

    ejercicios canvas Zab

---
+++ ejercicios.js
@@ -0,0 +1,11 @@
+let resist1 = (-1)
+let resist2 =  (5)
+let resist3 = (6)
+let resist4 = (3)
+
+function sumResistance (resist1, resist2, resist3, resist4) {
+    const sumaTotalOhms = (resist1+resist2+resist3+resist4) 
+    console.log( "la suma de las resistencias es :" $(sumaTotalOhms));
+}
+
+ 
